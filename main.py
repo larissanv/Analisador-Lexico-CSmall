93fb7c7d97e0b3f0b78b6830246796a7e7404425
Larissa <larissanevesviana@gmail.com>
2018-02-22 20:16:03 -0300

    adicionado codigo fonte a ser analisado

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main():
+	print "Analisador Lexico CSmall"  
+	arq = open('codigo-fonte.txt', 'r')
+	caracteres = list(arq.read())
+	print caracteres
+	linha = 0
+	
+if __name__ == "__main__":
+	main()
